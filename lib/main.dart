d7b8e6cbab3ad3c3eab6da9dadcd5393200a7a2f
doducduy20127477 <90464550+doducduy20127477@users.noreply.github.com>
2023-12-17 20:39:58 +0700

    feat: add colors & fonts

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,24 @@ void main() {
       appBar: AppBar(
         title: Text('my first app'),
         centerTitle: true,
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text('hello ninjas!'),
+        child: Text(
+            'hello ninjas!',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower'
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
         child: Text('click'),
+        backgroundColor: Colors.red[600],
       ),
     ),
   ));
