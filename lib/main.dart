76699b423cffb2a726ae5e6bb503922b9167d204
doducduy20127477 <90464550+doducduy20127477@users.noreply.github.com>
2023-12-30 20:39:46 +0700

    feat: add Card template

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,36 @@ class _HomeState extends State<Home> {
     Quote(author: 'Oscar Wilde', text: 'I have nothing to declare except my genius'),
     Quote(author: 'Oscar Wilde', text: 'The truth is rarely pure and never simple')
   ];
-  
+
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600]
+              ),
+            ),
+            SizedBox(height: 6.0),
+            Text(
+              quote.author,
+              style: TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.grey[800]
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +61,7 @@ class _HomeState extends State<Home> {
       ),
 
       body: Column(
-        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
 
     );
