b4b22dc25857d86f841a58138d47598bbea2b2fd
doducduy20127477 <90464550+doducduy20127477@users.noreply.github.com>
2024-01-14 16:27:04 +0700

    feat: add routes and navigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:myflutterapp/pages/choose_location.dart';
 import 'package:myflutterapp/pages/home.dart';
+import 'package:myflutterapp/pages/loading.dart';
 
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
   ));
 }
