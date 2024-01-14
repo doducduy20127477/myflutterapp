a52cc3c73c2b60ec1ba919f029eea9e5905b3577
doducduy20127477 <90464550+doducduy20127477@users.noreply.github.com>
2024-01-14 15:55:30 +0700

    feat: add basic screens and start the World time app

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:myflutterapp/quote.dart';
-import 'quote_card.dart';
+import 'package:myflutterapp/pages/home.dart';
+
 
 void main() {
   runApp(MaterialApp(
     home: Home(),
   ));
 }
-
-class Home extends StatefulWidget {
-  const Home({super.key});
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-
-  List<Quote> quotes = [
-    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken'),
-    Quote(author: 'Oscar Wilde', text: 'I have nothing to declare except my genius'),
-    Quote(author: 'Oscar Wilde', text: 'The truth is rarely pure and never simple')
-  ];
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Awesome Quotes'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-
-      body: Column(
-        children: quotes.map((quote) => QuoteCard(
-            quote: quote,
-            delete: () {
-              setState(() {
-                quotes.remove(quote);
-              });
-            }
-        )).toList(),
-      ),
-
-    );
-  }
-}
-
-
